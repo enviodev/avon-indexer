3843a1fc3f05111e98d7615435b7e0432ab75418
keenbeen32 <kenauvith@gmail.com>
2025-08-07 15:00:33 +0200

    chore: migrate schema graphql

--- test/Test.ts
+++ test/Test.ts
@@ -1,38 +1,25 @@
 import assert from "assert";
 import { 
-  TestHelpers,
-  OrderBook_BorrowOrderCanceled
+  TestHelpers
 } from "generated";
 const { MockDb, OrderBook } = TestHelpers;
 
-describe("OrderBook contract BorrowOrderCanceled event tests", () => {
+describe("OrderBook contract tests", () => {
   // Create mock db
   const mockDb = MockDb.createMockDb();
 
   // Creating mock for OrderBook contract BorrowOrderCanceled event
   const event = OrderBook.BorrowOrderCanceled.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
 
-  it("OrderBook_BorrowOrderCanceled is created correctly", async () => {
+  it("OrderBook BorrowOrderCanceled event is processed correctly", async () => {
     // Processing the event
     const mockDbUpdated = await OrderBook.BorrowOrderCanceled.processEvent({
       event,
       mockDb,
     });
 
-    // Getting the actual entity from the mock database
-    let actualOrderBookBorrowOrderCanceled = mockDbUpdated.entities.OrderBook_BorrowOrderCanceled.get(
-      `${event.chainId}_${event.block.number}_${event.logIndex}`
-    );
-
-    // Creating the expected entity
-    const expectedOrderBookBorrowOrderCanceled: OrderBook_BorrowOrderCanceled = {
-      id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-      borrower: event.params.borrower,
-      rate: event.params.rate,
-      ltv: event.params.ltv,
-      amount: event.params.amount,
-    };
-    // Asserting that the entity in the mock database is the same as the expected entity
-    assert.deepEqual(actualOrderBookBorrowOrderCanceled, expectedOrderBookBorrowOrderCanceled, "Actual OrderBookBorrowOrderCanceled should be the same as the expectedOrderBookBorrowOrderCanceled");
+    // The event should be processed without errors
+    // Since we removed the raw event entities, we just verify the event was processed
+    assert.ok(mockDbUpdated, "Event should be processed successfully");
   });
 });
